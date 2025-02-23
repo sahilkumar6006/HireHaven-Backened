4991582720fbabb78034e07e82b0a0eb15e95c36
sahil kumar <sahilgorka123@gmail.com>
2025-02-23 12:45:50 +0530

    fixed the setup

--- src/app.js
+++ src/app.js
@@ -16,17 +16,14 @@ app.use(cookieParser())
 
 
 //routes import
-import userRouter from './routes/user.routes.js'
+
 import healthcheckRouter from "./routes/healthcheck.routes.js"
-import adminVehicleRouter from "./routes/admin.vehicle.routes.js"
-import vehicleRouter from "./routes/vehicle.routes.js"
+
 
 
 //routes declaration
 app.use("/api/v1/healthcheck", healthcheckRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/admin/vehicles", adminVehicleRouter)
-app.use("/api/v1/vehicles", vehicleRouter)
+
 // http://localhost:8000/api/v1/healthcheck
 
 export { app }
